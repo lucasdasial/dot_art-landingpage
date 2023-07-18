6ca6f75b4abad03e931691d156c4b4cb3a5865e9
Lucas Alves <as.lucasalves@gmail.com>
2023-07-18 11:46:52 -0300

    update images

--- src/components/molecules/MiniGallery/index.tsx
+++ src/components/molecules/MiniGallery/index.tsx
@@ -20,7 +20,7 @@ export function MiniGallery() {
     >
       <ImageSquare srcImg="https://images.unsplash.com/photo-1585644198527-05519fdeaf98?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80" />
       <ImageSquare srcImg="https://images.unsplash.com/photo-1554139681-1adb48e035cb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=465&q=80" />
-      <ImageSquare srcImg="https://images.unsplash.com/photo-1547333590-47fae5f58d21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80" />
+      <ImageSquare srcImg="https://plus.unsplash.com/premium_photo-1672287578296-21a2e3504bf9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80" />
     </MiniGallery>
   );
 }
